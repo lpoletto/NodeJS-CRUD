2387d387d5a9b69c9014c4abbed1c9989060c5c1
lpoletto <48451077+lpoletto@users.noreply.github.com>
2020-10-05 12:49:53 -0300

    Se agregó solución a problemas de CORS

--- app.js
+++ app.js
@@ -25,6 +25,20 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+/** HEADER INICIO */
+app.use(function(req, res, next) {
+  res.setHeader('Access-Control-Allow-Origin','*');
+  res.setHeader('Access-Control-Allow-Methods','GET,POST,DELETE,PUT');
+  next();
+});
+app.options("/*", function(req, res, next){
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
+  res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, Content-Length, X-Requested-With,x-access-token');
+  res.send(200);
+});
+/** HEADER FIN */
+
 // Definición de rutas
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
